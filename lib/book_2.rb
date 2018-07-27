ffb77b1e0494ad35398973464235bfaa49f1d207
12helenl <12helenl@gmail.com>
2018-07-27 21:47:08 +0000

    automatically backed up by learn

---
+++ lib/book_2.rb
@@ -0,0 +1,15 @@
+# def turn_page
+#   puts "Flipping the page...wow, you read fast!"
+# end
+# end
+
+# book_2 = Book.new ("The World According to Garp")
+book.turn_page
+# end
+
+# "Agatha Cristie","272","Mystery"
+
+# puts book_1.title
+# puts book_1.author
+# puts book_1.page_count
+# puts book_1.genre
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
 # Make your shoe class here!
+
+class Shoe 
+  # (brand, color, size, material, condition)
+# attr_accessor :brand, :color, :size, :material, :condition
+ 
+attr_accessor :brand, :color, :size, :material, :condition
+
+def initialized (brand, color, size, material,condition)
+@brand = brand
+@color = color
+@size = size
+@material = material
+@condition = condition
+end
+
+def cobble
+  puts "Your shoe is as good as new"
+end
+end 
+
+shoe_1 = Shoe.new
+puts shoe_1.brand = "Adidas"
+puts shoe_1.color = "Red"
+puts shoe_1.size = "7"
+puts shoe_1.material = "Leather"
+puts shoe_1.condition = "New"
+shoe_1.cobble
